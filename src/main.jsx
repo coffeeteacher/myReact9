561b88d96b4669d9796c98ee658f7e234eb394b2
coffeeteacher <coffeeteacher@gmail.com>
2025-08-06 16:22:55 +0800

    0806-2-card

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 // import './index.css'
 // import App from './App.jsx'
-import App from './pages/App0806-2.jsx'
+import App from './pages/App0806-2-card.jsx'
 
 
 createRoot(document.getElementById('root')).render(
