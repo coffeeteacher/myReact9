b5e43b1ebf118e2b1396d7207eedab0577ecdc3c
coffeeteacher <coffeeteacher@gmail.com>
2025-08-13 10:53:15 +0800

    0813-1-weatherComponent

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 // import './index.css'
 // import App from './App.jsx'
-import App from './pages/App0812-weather2'
-
+// import './sass/all.scss'
+import App from './pages/App0813-weather3'
 
 createRoot(document.getElementById('root')).render(
   // 嚴謹模式
