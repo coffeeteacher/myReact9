0291607998566434cccf2f467560f443eb3059ff
coffeeteacher <coffeeteacher@gmail.com>
2025-08-13 16:03:45 +0800

    0813-nav2

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom'
 
 const Nav = () => {
   return (
-    <div>
+    <div>      
       <ul>
         <li><Link to="/">HOME</Link></li>        
         <li><Link to="/about">ABOUT</Link></li>
