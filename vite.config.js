3d05234de2ccb467fe9c10a15211b97e2ff813bd
coffeeteacher <coffeeteacher@gmail.com>
2025-10-01 10:54:38 +0800

    edit vite.config-1001

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-  base:"./",
-  plugins: [react()],
+export default defineConfig({  
+  build:{
+    // 修改文件大小限制
+    chunkSizeWarningLimit:1500,
+  },  
+  base:"./",  // 修改執行路徑
+  plugins: [react()],  
 })
